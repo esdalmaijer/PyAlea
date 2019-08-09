edff3639b0ae8d7fb499a1211a24bf1a9737bcc7
Edwin Dalmaijer <edwin.dalmaijer@gmail.com>
2019-08-09 14:30:54 +0100

    Updated to package for 'import alea' and to include DLL in package directory

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import alea
 import setuptools
 
 with open("README.md", "r") as fh:
@@ -5,16 +6,19 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="python-alea",
-    version="0.0.2",
+    version=alea.__version__,
     author="Edwin Dalmaijer",
     author_email="edwin.dalmaijer@gmail.com",
     description="Python wrapper for the IntelliGaze API by Alea Technologies",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/esdalmaijer/PyAlea",
-    packages=setuptools.find_packages(),
-    data_files=[("python-alea",["./python-alea/EtApi.dll","./python-alea/CEtAPI.dll","./python-alea/CEtAPIx64.dll"])],
+    packages=["alea"],
+    include_package_data=True,
+    package_data={"alea":["*.dll"]},
     classifiers=[
+        "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
         "Programming Language :: Python :: 2",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: Microsoft :: Windows",
